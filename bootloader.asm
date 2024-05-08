1d47fe7c03bc48d41fd3c285b5865230f35138b1
Nanaimo_2013 <116044506+Nanaimo2013@users.noreply.github.com>
2024-05-08 14:14:31 -0700

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -12,9 +12,25 @@ start:
     mov ax, 0x07C0
     mov ds, ax
 
-    ; Your bootloader code here
-
-    jmp $
+    jmp kernel_start
 
     times 510-($-$$) db 0
     dw 0xAA55
+
+kernel_start:
+    ; Print message
+    mov si, message        ; Load address of message into SI
+print_loop:
+    lodsb                  ; Load byte from SI into AL and increment SI
+    test al, al            ; Check if AL is zero (end of string)
+    jz end_print           ; If zero, end of string
+    mov ah, 0x0E           ; BIOS teletype function
+    int 0x10               ; Call BIOS interrupt
+    jmp print_loop         ; Repeat until end of string
+end_print:
+
+    ; Infinite loop
+    jmp $
+    
+message:
+    db 'Welcome to NansStudioOS!', 0   ; Null-terminated message
